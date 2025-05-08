1d4c5a6ace28cb607df2388bb5a08ef459c844e3
Leonardo Andrade <leonardofandrade@gmail.com>
2025-05-08 13:07:23 -0300

    Aula 2.15

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ let firstName = "Leonardo"; // Variável que pode ser alterada
 let lastName = "Andrade"; // Variável que pode ser alterada
 console.log(firstName); // Saída: Leonardo
 console.log(lastName); // Saída: Andrade
+
+const age = 30; // Variável que não pode ser alterada
+// age = 20; // Isso causará um erro, pois age é uma constante
+console.log(age); // Saída: 30
