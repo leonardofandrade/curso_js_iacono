e6141b808f60cee510cdeae19762b9ec0052b17d
Leonardo Andrade <leonardofandrade@gmail.com>
2025-05-08 13:04:49 -0300

    Add initial HTML structure, JavaScript functionality, and empty CSS file

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+
+//Declaração de variáveis pode ser feita com var, let ou const
+let firstName = "Leonardo"; // Variável que pode ser alterada
+let lastName = "Andrade"; // Variável que pode ser alterada
+console.log(firstName); // Saída: Leonardo
+console.log(lastName); // Saída: Andrade
