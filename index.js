c52bd149718edb306274225eac6ea86134ab1808
Leonardo Andrade <leonardofandrade@gmail.com>
2025-05-08 13:15:16 -0300

    aula 2.17

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ console.log(age); // Saída: 30
 //ASI - Automatic Semicolon Insertion
 // O JavaScript insere ponto e vírgula automaticamente em algumas situações
 console.log("Hello, ASI!") // Saída: Hello, World!
+
+//Variáveis Primitivas
+let number = 10; // Número
+console.log(number); // Saída: 10
+console.log(typeof number); // Saída: number
+// Variáveis de tipo primitivo são imutáveis, ou seja, não podem ser alteradas
+// Exemplo: number = 20; // Isso causará um erro, pois number é uma variável primitiva
+let string = "Hello, World!"; // String
+console.log(string); // Saída: Hello, World!
+console.log(typeof string); // Saída: string
+
+let boolean = true; // Booleano
+console.log(boolean); // Saída: true
+console.log(typeof boolean); // Saída: boolean
+
+let bigInt = 1234567890123456789012345678901234567890n; // BigInt
+console.log(bigInt); // Saída: 1234567890123456789012345678901234567890n
+console.log(typeof bigInt); // Saída: bigint
+
+let symbol = Symbol("description"); // Símbolo
+console.log(symbol); // Saída: Symbol(description)  
+console.log(typeof symbol); // Saída: symbol
+
+let undefinedVar; // Indefinido
+console.log(undefinedVar); // Saída: undefined  
+console.log(typeof undefinedVar); // Saída: undefined
+
+string = "Testando"; // Alterando o valor da variável string
+console.log(string); // Saída: Testando
