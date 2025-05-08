10db8660a43dda5943064149ae9043cb21e55876
Leonardo Andrade <leonardofandrade@gmail.com>
2025-05-08 13:11:14 -0300

    aula 2.16

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ console.log(lastName); // Saída: Andrade
 const age = 30; // Variável que não pode ser alterada
 // age = 20; // Isso causará um erro, pois age é uma constante
 console.log(age); // Saída: 30
+
+//ASI - Automatic Semicolon Insertion
+// O JavaScript insere ponto e vírgula automaticamente em algumas situações
+console.log("Hello, ASI!") // Saída: Hello, World!
